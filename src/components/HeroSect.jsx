bb5067c77d81ab4dc5ea5fddfbb6cc014f9f8947
IrfanNaikwade <irfannayakvade2230@gmail.com>
2024-12-29 00:18:27 +0530

    Fixed Issues...

--- src/components/HeroSect.jsx
+++ src/components/HeroSect.jsx
@@ -1,40 +1,50 @@
-import heroIllustration from "../assets/images/Hero-illustrations.png";
+import heroIllustration from "../assets/images/Hero-illustrations.svg";
 import linkedin from "../assets/icons/linkedin.svg";
 import github from "../assets/icons/github.svg";
-import scrollIcon from  "../assets/icons/scroll-animation.svg";
+import scrollIcon from "../assets/icons/scroll-animation.svg";
 export const HeroSect = () => {
   return (
     <>
-      <div className="hero-section flex justify-between relative h-full">
-        <div className="heroText h-full">
+      <div className="hero-section w-full h-full flex justify-between relative">
+        <div className="heroText h-full w-full z-30">
           <div className="flex flex-col text-white pl-5">
             <span className="font-semibold text-2xl">Hi! I&apos;m Irfan,</span>
-            <span className="font-semibold text-2xl">A Web <span className="bg-fontBlue text-transparent bg-clip-text">Developer</span></span>
+            <span className="font-semibold text-2xl">
+              A Web{" "}
+              <span className="bg-fontBlue text-transparent bg-clip-text">
+                Developer
+              </span>
+            </span>
             <span className="text-[#989898] font-normal text-[10px]">
-              Combining development skills with creative design, <br />I transform
-              concepts into dynamic, user-centered <br /> web solutions.
+              Combining development skills with creative design, <br />I
+              transform concepts into dynamic, user-centered <br /> web
+              solutions.
             </span>
           </div>
           <div className="social-icons flex w-8 gap-3 h-20 ml-5">
-             <img className="w-7" src={linkedin} alt="" />
-             <img src={github} alt="" />
+            <img className="w-7" src={linkedin} alt="" />
+            <img src={github} alt="" />
           </div>
-          <div className="scroll-icon absolute bottom-52 w-full flex flex-col items-center">
-            <img className="w-5" src={scrollIcon} alt="" />
-          </div>
-          <div className="buttons absolute bottom-20 w-full flex flex-col items-center text-white mt-3 gap-3 text-[11px] tracking-widest">
-                <button className="bg-[#6A57E0] hover:bg-[#563ef4] w-[70vw] py-3 rounded-3xl uppercase text-center font-semibold cursor-pointer">Explore Projects</button>
-                <button className="text-[#80FFD2] border-2 border-[#80FFD2] hover:bg-[#80FFD2] hover:text-[#161B23] rounded-3xl w-[70vw] py-3 uppercase text-center font-semibold cursor-pointer">
-                  Get in Touch
-                </button>
+          <div className="buttons absolute w-full bottom-10 flex items-center flex-col text-white gap-3 text-[10px] tracking-widest">
+            <button className="bg-color1 hover:bg-color1Hover w-2/3 py-3 rounded-3xl uppercase text-center font-semibold cursor-pointer">
+              Explore Projects
+            </button>
+            <button className="text-color2 border-2 border-color2 hover:bg-color2 hover:text-color3 rounded-3xl w-2/3 py-3 uppercase text-center font-semibold cursor-pointer">
+              Get in Touch
+            </button>
           </div>
         </div>
-        <div className="heroIllustration absolute w-full z-10">
+        <div className="heroIllustration flex flex-col items-center w-full h-full absolute">
           <img
-            className="w-[90%] absolute top-10 left-8 right-0"
+            className="min-w-[100vw] absolute top-6 left-14"
             src={heroIllustration}
             alt=""
           />
+          <div className="absolute bottom-36">
+            <div className="scroll-icon">
+              <img className="w-5" src={scrollIcon} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
         deepBlue:"linear-gradient(90deg, #161B23 0%, #1C212A 25%, #1C212A 50%, #1C212A 75%, #1C212A 100%)",
         fontBlue:"linear-gradient(50deg, #B597F6 0%, #96C6EA 100%)",
       },
+      colors:{
+        color1:"#6A57E0",
+        color1Hover:"#563ef4",
+        color2:"#80FFD2",
+        color3:"#161B23",
+      },
       fontFamily:{
         sora:["Sora","sans"]
       }
