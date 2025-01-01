fdd449a04d306b2852491b14c01036283c958cee
IrfanNaikwade <irfannayakvade2230@gmail.com>
2025-01-01 14:58:52 +0530

    Added Contact Us page and Footer

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -1,3 +1,5 @@
+import msgIcon from "../assets/icons/mail.png"
+import { Footer } from "./footer";
 export const ContactUs = () => {
   return (
     <>
@@ -8,18 +10,50 @@ export const ContactUs = () => {
             Get <br /> in <br /> Touch
           </p>
         </div>
-        <div className="form h-screen">
-          <div className="px-3 mt-20">
+        <div className="form h-screen py-10 px-3 flex flex-col">
+          <div className="illustration w-full flex justify-center items-center h-1/4">
+            <img className="w-3/4" src={msgIcon} alt="" />
+          </div>
+          <div className="flex flex-col gap-8 py-10 px-3 w-full h-full rounded-xl">
             <div className="input-name w-full">
               <input
-                className="bg-transparent border-color2 border-2 rounded-3xl py-2 px-3 w-full placeholder:text-color2/60 pl-5"
+                className="bg-transparent border-color2 border-b py-2 px-3 w-full placeholder:text-color2/60 pl-3 focus:outline-none text-color2"
                 type="text"
                 name="name"
                 placeholder="Enter Your Name"
               />
             </div>
+            <div className="input-name w-full">
+              <input
+                className="bg-transparent border-color2 border-b py-2 px-3 w-full placeholder:text-color2/60 pl-3 focus:outline-none text-color2"
+                type="text"
+                name="name"
+                placeholder="Enter Your Email ID"
+              />
+            </div>
+            <div className="input-name w-full">
+              <input
+                className="bg-transparent border-color2 border-b py-2 px-3 w-full placeholder:text-color2/60 pl-3 focus:outline-none text-color2"
+                type="text"
+                name="name"
+                placeholder="Enter Your Phone Number"
+              />
+            </div>
+            <div className="input-name w-full">
+              <textarea
+                rows={5}
+                className="bg-transparent border-color2 border-b rounded-none py-2 px-3 w-full placeholder:text-color2/60 pl-3 focus:outline-none text-color2 resize-none"
+                type="text"
+                name="name"
+                placeholder="Subject"
+              />
+            </div>
+            <button className="bg-color2 text-color3 hover:bg-color2/80 hover:scale-105 transition duration-200 rounded-xl text-center font-semibold cursor-pointer w-2/3 h-[5vh] tracking-wide self-center">
+              Send it Now
+            </button>
           </div>
         </div>
+      <Footer />
       </div>
     </>
   );
---
+++ src/components/footer.jsx
@@ -0,0 +1,33 @@
+import github from "../assets/icons/github.svg"
+import linkedin from "../assets/icons/linkedin.svg"
+import telegram from "../assets/icons/telegram.svg"
+export const Footer = () =>{
+    return(
+        <>
+            <div className="flex flex-col w-full my-20">
+                <div className="nav-items w-full flex justify-center gap-5 text-white text-center text-xs font-light uppercase">
+                    <div className="nav-option">Home</div>
+                    <div className="nav-option">About Us</div>
+                    <div className="nav-option">Projects</div>
+                    <div className="nav-option">Blog</div>
+                </div>
+                <div className="icons w-full flex justify-center mt-5 items-center gap-5">
+                    <div className="icon">
+                        <img src={github} alt="" />
+                    </div>
+                    <div className="icon">
+                        <img src={linkedin} alt="" />
+                    </div>
+                    <div className="icon">
+                        <img src={telegram} alt="" />
+                    </div>
+                </div>
+                <div className="text-white font-extralight text-xs text-center my-5">
+                    <div className="title">©2025 Irfan Naikwade. All rights reserved.</div>
+                    <div className="title">Bringing ideas to life with code.</div>
+                    <div className="title">Thanks for visiting!.</div>
+                </div>
+            </div>
+        </>
+    )
+}
